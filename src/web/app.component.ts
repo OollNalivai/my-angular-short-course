0573639d2fb21c06c99e06d40ef667f1aa20884d
VladislavOoll <apahiphip@mail.ru>
2022-10-04 23:36:07 +0300

    feat: Input, pipe, interpolation

--- src/web/app.component.ts
+++ src/web/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductInterface } from '../models/interfaces/product.interface';
+import { products as data} from './data/productrs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-angular-short-course';
+
+  products: ProductInterface[] = data
 }
