1aea9e57b05c0c4baf2bfa112a65658b30764eee
VladislavOoll <apahiphip@mail.ru>
2022-10-07 23:30:43 +0300

    feat: message loading products, method getAll products

--- src/web/app.component.ts
+++ src/web/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductInterface } from '../models/interfaces/product.interface';
-import { products as data} from './data/productrs';
+import { ProductsService } from './pages/home/components/product/services/products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-angular-short-course';
+export class AppComponent implements OnInit{
 
-  products: ProductInterface[] = data
+  title = 'my-angular-short-course';
+  products: ProductInterface[] = []
+  loading = false
+
+  constructor(private productsService: ProductsService) {
+  }
+
+  ngOnInit(): void {
+    this.loading = true
+    this.productsService.getAll().subscribe( products => {
+      console.log(products)
+      this.products = products
+      this.loading = false
+    })
+  }
+
 }
--- src/web/pages/home/components/product/services/products.service.ts
+++ src/web/pages/home/components/product/services/products.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { delay, Observable } from 'rxjs';
+import { ProductInterface } from '../../../../../../models/interfaces/product.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ProductsService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<ProductInterface[]> {
+    return this.http.get<ProductInterface[]>('https://fakestoreapi.com/products', {
+      params: new HttpParams({
+          fromObject: {
+            limit: 5
+          }
+        }
+      )
+    }).pipe(delay(1500));
+  }
 
 }
